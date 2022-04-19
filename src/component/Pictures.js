f06e26e6706bd28626456603d56919bd1e58911e
monikasingh95 <78414329+monikasingh95@users.noreply.github.com>
2022-04-19 16:49:40 +0530

    Generate meme Api

--- src/component/Pictures.js
+++ src/component/Pictures.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from 'react';
 import axios from "axios";
+import Buffer from "buffer";
 
 
 function Pictures() {
@@ -8,9 +9,25 @@ function Pictures() {
         fetchPictures();
     }, [])
     useEffect(() => {
-        console.log(pictures)
+        // console.log(pictures);
+
+        // let base64ImageString = Buffer.from(pictures, 'binary').toString('base64')
+        //
+        if(typeof pictures === 'string'){
+            // let enc = new TextEncoder(); // always utf-8
+            // let encPicture = enc.encode(pictures);
+
+            // let base64ImageString = Buffer.from(encPicture, 'binary').toString('base64');
+            let srcValue = "data:image/jpeg;base64,"+pictures;
+            console.log(srcValue + "##xxxx");
+        }
+
+        // let binaryData = [];
+        // binaryData.push(pictures);
+        // console.log(window.webkitURL.createObjectURL(new Blob([pictures], {type: "image/jpeg"})));
+
     }, [pictures])
-    const fetchPictures = async (topText, bottomText) => {
+    const fetchPictures = async () => {
 
         let config = {
             method: 'get',
@@ -23,21 +40,41 @@ function Pictures() {
 
         const response = await axios(config);
         setPictures(response.data)
+        console.log(response);
     }
 
 
     let x = 1;
+
+    //let b64 = window.btoa(unescape(encodeURIComponent(pictures)));
+
+    //let b64Response = btoa(pictures);
+    //let outputImg = document.createElement('img');
+    //outputImg.src = 'data:image/png;base64,'+b64Response;
+
+    //document.body.appendChild(outputImg);
+
+
+
+    // const urlCreator = window.URL || window.webkitURL;
+    // console.log(urlCreator.createObjectURL(pictures));
+    // document.getElementById('myImage').src = urlCreator.createObjectURL(pictures);
+
+    let srcValue = "data:image/jpeg;base64,"+pictures;
+
     return (
         <div className="Pictures">
             {
-                pictures && pictures.map(picture=>{
-                    return(
-                        <div key={x++} style={{alignItems:'center',margin:'20px 60px'}}>
-                            <h4>{picture}</h4>
-                        </div>
-                    )
+                // pictures && pictures.map(picture=>{
+                //     return(
+                //         <div key={x++} style={{alignItems:'center',margin:'20px 60px'}}>
+                //             <h4>{picture}</h4>
+                //         </div>
+                //     )
+                //
+                // })
+                <img src={srcValue} />
 
-                })
             }
         </div>
     );
